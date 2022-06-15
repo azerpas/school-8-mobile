a4d35d9eb973fe12ecb477aba25c368532420678
Azerpas <anthony@smbs.io>
2022-06-15 14:46:28 +0000

    Started to work on JDBC

--- app/src/main/java/com/example/tennistracker/PlayersTask.java
+++ app/src/main/java/com/example/tennistracker/PlayersTask.java
@@ -29,5 +29,38 @@ public class PlayersTask extends AsyncTask<String, Integer, Void> {
     protected void onPostExecute(Long result) {
         // Call the result handler at the end
     }
+    /*
+    public void fetchPlayers() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Connection connection = DriverManager.getConnection("jdbc:postgresql://10.0.2.2:5234/TennisTracker", "root", "root");
+
+                    String sql = "SELECT * FROM Players";
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    ResultSet resultSet = statement.executeQuery();
+                    while (resultSet.next()) {
+                        Player p = new Player(
+                            resultSet.getString("id"), 
+                            resultSet.getString("first"),
+                            resultSet.getString("last"), 
+                            resultSet.getString("hand"), 
+                            resultSet.getString("dob"), 
+                            resultSet.getString("ioc"), 
+                            resultSet.getString("height"),
+                            resultSet.getString("wikidata")
+                        );
+                    }
+                    players.Add(p);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                
+            }}).start();
+    } 
+    */
 }
 
